36b840696da0ddb00c3c3c26e304c89a92f17040
ZERODARK369 <102336416+ZERODARK369@users.noreply.github.com>
2022-03-24 20:50:33 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showIcon(IconNames.Square)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
